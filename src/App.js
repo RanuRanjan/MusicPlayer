f3c8da99ed8c3e9bed914029773d2213a25eea69
RanuRanjan <ranuranjan25@gmail.com>
2022-01-15 21:03:25 +0530

    App js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Navba from './components/Navbar';
+import Ritviz from './components/Artists/Ritviz';
+import Pritam from './components/Artists/Pritam';
+import Eminem from './components/Artists/Eminem';
+import Mithun from './components/Artists/Mithun';
+import Papon from './components/Artists/Papon';
+import Badshah from './components/Artists/Badshah';
+import {Route, Switch } from  'react-router-dom';
 
 function App() {
+  // const  songs= async function() {
+  //     let res= await fetch("http://localhost:3000/HipHop")
+  //     let data= res.json()
+  //     console.log(data);
+
+  
   return (
-    <div className="App">
-       <Navbar/>
-    </div>
+    <>
+       <Switch>
+         <Route path='/' exact>
+            <Home/>
+         </Route>
+         <Route path='/ritviz' exact>
+            <Ritviz/>
+         </Route>
+         <Route path='/badshah' exact>
+            <Badshah/>
+         </Route>
+         <Route path='/papon' exact>
+            <Papon/>
+         </Route>
+         <Route path='/eminem' exact>
+            <Eminem/>
+         </Route>
+         <Route path='/pritam' exact>
+            <Pritam/>
+         </Route>
+         <Route path='/mithun' exact>
+            <Mithun/>
+         </Route>
+         </Switch>
+    </>
   );
 }
 
